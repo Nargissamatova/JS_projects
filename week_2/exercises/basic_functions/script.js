1b519e1d126c8ef389fc685b9c1e7ae168b41340
Nargis <nargissamatova16@gmail.com>
2024-01-20 19:59:52 +0200

    Completed exercises on week 2

---
+++ week_2/exercises/basic_functions/script.js
@@ -0,0 +1,15 @@
+// Write the function sum such that it returns the sum of a and b.
+function sum(x, y) {
+  return x + y;
+}
+// sample usage - do not modify
+console.log(sum(1, 3));
+console.log(sum(2, 5));
+
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+function multiply(x, y) {
+  return x * y;
+}
+// sample usage - do not modify
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
---
+++ week_2/exercises/operators/scripts.js
@@ -0,0 +1,21 @@
+/* 
+- Create variables a and b, and assign them integer values.
+- Use add, subtraction, multiply, divide, and modulus operator for a and b. Store each result in separate variables (sum, difference, product, quotient, reminder).
+- Calculate the remainder of a divided by b using the modulus operator and store it in a variable called remainder.
+- Use console.log() to print the results of each operation to the console.
+- Add comments in your code to explain what each operation does. */
+
+let a = 4;
+let b = 3;
+
+let sum = a + b; // adds
+let difference = a - b; //difference
+let product = a * b; //product
+let quotient = a / b; //quotient
+let reminder = a % b; //reminder
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(reminder);
