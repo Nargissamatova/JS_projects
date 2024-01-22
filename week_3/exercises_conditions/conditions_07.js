cf008fdd01076555de41bf5dec699ede58a7843f
Nargis <nargissamatova16@gmail.com>
2024-01-22 18:49:18 +0200

    Completed task 7

--- week_3/exercises_conditions/conditions_07.js
+++ week_3/exercises_conditions/conditions_07.js
@@ -10,18 +10,26 @@ Returns a letter grade for the score based on these criteria:
 - 'F' for scores below 60.
 
 */
-function assignGrade(grade) {
-  switch (grade) {
-    case grade >= 90:
-      "A";
+function assignGrade(score) {
+  let grade;
+  switch (true) {
+    case score >= 90:
+      grade = "A";
       break;
-    case grade >= 90:
-      "A";
+    case score >= 80:
+      grade = "B";
+      break;
+    case score >= 70:
+      grade = "C";
+      break;
+    case score >= 60:
+      grade = "D";
       break;
 
     default:
-      break;
+      grade = "F";
   }
+  return grade;
 }
 
 // Sample usage - do not modify
