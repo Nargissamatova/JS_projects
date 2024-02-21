86fa1dd21470c2c83cf4bb88d5b77cbf038cba95
Nargis <nargissamatova16@gmail.com>
2024-02-21 19:38:09 +0200

    functions/data types/numbers/operators

--- week_2/exercises/basic_functions/script.js
+++ week_2/exercises/basic_functions/script.js
@@ -1,4 +1,4 @@
-// Write the function sum such that it returns the sum of a and b.
+// Write the function sum such that it returns the sum of a and b
 function sum(x, y) {
   return x + y;
 }
@@ -6,7 +6,7 @@ function sum(x, y) {
 console.log(sum(1, 3));
 console.log(sum(2, 5));
 
-// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b
 function multiply(x, y) {
   return x * y;
 }
