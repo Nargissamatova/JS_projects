fd5e661fbd065afb1f463854eedb3c4918d4c143
Nargis <nargissamatova16@gmail.com>
2024-03-07 22:22:35 +0200

    tricks/cars/try_catch

--- week_7/tricks/main.js
+++ week_7/tricks/main.js
@@ -4,26 +4,10 @@ const mobButton = document.querySelector(".mobile");
 const nav = document.querySelector("nav ul");
 const backToTopButton = document.querySelector("#backToTop");
 const menuItems = document.querySelectorAll("nav ul li a");
+const modalButton = document.querySelector(".modalButton");
+const overlay = document.querySelector(".overlay");
+const closeButton = document.querySelector(".closeButton");
 
-backToTopButton.addEventListener("click", () => {
-  document.body.scrollTop = 0; // for safari
-  document.documentElement.scrollTop = 0; // for chrome and others
-
-  header.classList.remove("bg");
-});
-
-/*
-OR
-
-const getToTop = () => {
-  document.body.scrollTop = 0; // for mac
-  document.documentElement.scrollTop = 0; // and diff browsers
-};
-
-backToTopButton.addEventListener("click", getToTop);
- */
-
-// Apear/ disappear when scrolling 200px
 window.onscroll = function () {
   scrollFunction();
 };
@@ -45,5 +29,29 @@ const scrollFunction = () => {
 const toggleMenu = () => {
   nav.classList.toggle("responsive");
 };
+
+//Modal and overlay
+const toggleModal = () => {
+  overlay.classList.toggle("visible");
+};
+
+/*
+const getToTop = () => {
+  document.body.scrollTop = 0; // for mac
+  document.documentElement.scrollTop = 0; // and diff browsers
+};
+
+backToTopButton.addEventListener("click", getToTop);
+ */
+backToTopButton.addEventListener("click", () => {
+  document.body.scrollTop = 0; // for safari
+  document.documentElement.scrollTop = 0; // for chrome and others
+
+  header.classList.remove("bg");
+});
+// Apear/ disappear when scrolling 200px
 mobButton.addEventListener("click", toggleMenu);
 menuItems.forEach((item) => item.addEventListener("click", toggleMenu));
+modalButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+//overlay.addEventListener("click", toggleModal);
